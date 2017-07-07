1ab4bd3cfc075cf16f44d60ec39d3a2499539971
willnickerson <wnickers@willamette.edu>
2017-07-07 15:46:04 -0700

    hamburger menu is no trigger fro acordian nav to open and close

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import HamburgerMenu from 'react-hamburger-menu';
+import Collapsible from 'react-collapsible';
 
 class Header extends Component {
   constructor(props) {
@@ -19,17 +20,35 @@ class Header extends Component {
 
   render() {
     return (
-      <HamburgerMenu
-        isOpen={this.state.open}
-        menuClicked={this.handleClick.bind(this)}
-        width={18}
-        height={15}
-        strokeWidth={1}
-        rotate={0}
-        color='white'
-        borderRadius={0}
-        animationDuration={0.5}
-      />
+      <div>
+        <Collapsible 
+          trigger={<HamburgerMenu
+            isOpen={this.state.open}
+            menuClicked={this.handleClick.bind(this)}
+            width={18}
+            height={15}
+            strokeWidth={1}
+            rotate={0}
+            color='white'
+            borderRadius={0}
+            animationDuration={0.5}
+          />}
+        >
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </ul>
+        </Collapsible>
+        {/*{this.state.open ? 
+        <ul>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Contact</li>
+          <li>Github</li>
+          <li>Twitter</li>
+        </ul> : null}*/}
+      </div>
     );
   }
 }
