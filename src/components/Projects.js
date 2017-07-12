6f6e5ddd490eddefb0c71b39c68a496e65c06629
willnickerson <wnickers@willamette.edu>
2017-07-12 14:31:36 -0700

    basic carousel styled and working with projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,6 +4,7 @@ import Slider from 'react-slick';
 class Projects extends Component {
   render() {
     const settings = {
+      arrows: true,
       dots: true,
       infinite: true,
       speed: 500,
@@ -12,12 +13,19 @@ class Projects extends Component {
     };
     return (
       <Slider {...settings}>
-        <div><h3>1</h3></div>
-        <div><h3>2</h3></div>
-        <div><h3>3</h3></div>
-        <div><h3>4</h3></div>
-        <div><h3>5</h3></div>
-        <div><h3>6</h3></div>
+        <div>
+          {/* eventually but sub slider in here */}
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur incidunt, sit cumque quaerat enim laborum accusamus totam reiciendis unde distinctio, eveniet officia temporibus voluptatibus aliquid consequuntur earum ad quam veritatis.</p> 
+          <img src="http://res.cloudinary.com/lejipni8p/image/upload/c_crop,g_north,h_1310,w_2560/v1490814219/Screen_Shot_2017-03-24_at_5.03.32_PM_gsp3re.png" alt="#"/>
+        </div>
+        <div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus facilis et quidem ipsam veritatis exercitationem omnis similique ratione, inventore minima accusamus! Quas ab, ipsam deserunt provident ad quis quae aliquid!</p>
+          <img src="http://res.cloudinary.com/lejipni8p/image/upload/c_crop,g_north,h_1310,w_2560/v1490814197/Screen_Shot_2017-03-24_at_5.05.17_PM_gunpnq.png" alt="#"/>
+        </div>
+        <div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae fugit ipsum sed iure doloribus, expedita ea et saepe eos minus exercitationem est repudiandae molestiae neque ducimus officia, magni ex in.</p>
+          <img src="http://res.cloudinary.com/lejipni8p/image/upload/c_crop,g_north,h_1310,w_2560/v1499893316/Screen_Shot_2017-07-12_at_2.00.57_PM_x63cpk.png" alt="#"/>
+        </div>
       </Slider>
     );
   }
